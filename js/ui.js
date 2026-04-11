8f4f8f4feeef599bf461ddb9fbf3fd94cf024c66
EvgenGnyp2011 <evgengnyp84@email.com>
2026-04-11 11:51:51 +0300

    ui.js content added

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,15 @@
+function renderBoard() {
+    let state = getGameState();сом
+    let boardElement = document.querySelector(".grid");
+    let html = '';
 
+    state.forEach(row => {
+        row.forEach(tile => {
+            html += `<div class="tile status-${tile}"></div>`;
+        });
+    });
+
+    if (boardElement) {
+        boardElement.innerHTML = html;
+    }
+};
